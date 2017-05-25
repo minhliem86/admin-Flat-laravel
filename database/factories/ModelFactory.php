f658d7622097894d45c347e050b2a216f638268a
minhliem86 <minhliemphp@gmail.com>
2017-05-25 21:40:05 +0700

    fix conflict

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,14 +22,8 @@ use Faker\Generator as Faker;
 //     ];
 // });
 
-<<<<<<< HEAD
-/*DEFINE PROJECT*/
-$factory->define(Project, function(Faker $faker){
-  return[
-=======
 $factory->define(Project::class, function(Faker  $faker){
   return [
->>>>>>> 8f5dcd71ba1445e662a3ff499dedf54653ad6e58
     'video_id' => str_random(6),
     'title' => $faker->sentence(),
     'description' => $faker->paragraph(),
